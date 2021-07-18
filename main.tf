f1fa31b56ee887676c828ade4e15d61c35b6b1e9
jgourmelen <jgourmelen@gmail.com>
2021-07-18 16:21:31 +0200

    Add comment

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "metric1" {
   source = "./terraform-gcloud-metric"
+  # project-name = "geek-my-job"  ==> valeur par défault dans le module
+  # filter-name = "geek-my-filter" ==> valeur par défault dans le module
 }
 
 module "metric2" {
